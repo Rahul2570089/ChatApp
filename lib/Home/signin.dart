156aa7b8ea052e869e98118fc6f4bd44906aefec
rahul <rs2570089@gmail.com>
2022-01-16 12:18:35 +0530

    App added

---
+++ lib/Home/signin.dart
@@ -0,0 +1,144 @@
+// ignore_for_file: use_key_in_widget_constructors, prefer_const_constructors_in_immutables
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:mychatapp/helper/helper.dart';
+import 'package:mychatapp/services/auth.dart';
+import 'package:mychatapp/services/database.dart';
+
+import 'mainscreen.dart';
+
+class SignIn extends StatefulWidget {
+
+  final Function toggle;
+  SignIn(this.toggle);
+
+  @override
+  State<SignIn> createState() => _SignInState();
+}
+
+class _SignInState extends State<SignIn> {
+  bool loading = false;
+  final check = GlobalKey<FormState>();
+  DataBaseMethods dataBaseMethods = DataBaseMethods();
+  TextEditingController emailTextEditer = TextEditingController();
+  TextEditingController passwordEditer = TextEditingController();
+  final AuthMethod authmethod = AuthMethod();
+  QuerySnapshot? snapshot;
+  
+  InputDecoration textdecoration(String hint) {
+    return InputDecoration(
+      filled: true,
+        hintText: hint,
+        hintStyle: const TextStyle(color: Colors.black45),
+        enabledBorder: const OutlineInputBorder(
+            borderSide: BorderSide(
+          color: Colors.green,
+        )),
+        focusedBorder: const OutlineInputBorder(
+            borderSide: BorderSide(
+          color: Colors.green,
+        )));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Sign In"),
+        backgroundColor: Colors.green[300],
+      ),
+      backgroundColor: Colors.green[100],
+      body: loading ? const Center(child: CircularProgressIndicator()) : Center(
+        child: SingleChildScrollView(
+          child: Container(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Form(
+                  key: check,
+                  child: Column (
+                    children: [
+                    TextFormField(
+                      validator: (val) { return val!.isEmpty ? "Please enter the email" : null;},
+                      controller: emailTextEditer,
+                      decoration: textdecoration("Email"),
+                    ),
+                    TextFormField(
+                      validator: (val) { return val!.isEmpty ? "Please enter the password" : null;},
+                      controller: passwordEditer,
+                      obscureText: true,
+                      decoration: textdecoration("Password"),
+                    ),
+                    const SizedBox(height: 18,),
+                    const SizedBox(height: 25,),
+                    Container(
+                      color: Colors.green,
+                      height: 50,
+                      width: MediaQuery.of(context).size.width,
+                      child: ElevatedButton(
+                        onPressed: () async {
+
+                        if(check.currentState!.validate()) {
+                          setState(() {
+                            loading=true;
+                          });
+                          dataBaseMethods.getUserbyemail(emailTextEditer.text).then((value) {
+                              snapshot = value;
+                              helpermethod.saveusernameloggedinsharedpreference(snapshot?.docs[0]["Name"]);
+                            
+                          });
+            
+                          authmethod.signinwithemailpassword(emailTextEditer.text,passwordEditer.text).then((value) async {
+                            if(value!=null) {
+                              helpermethod.saveuserloggedinsharedpreference(true);
+                              Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => const HomeScreen() ));
+                            }
+                          });
+                        }
+                        else {
+                          setState(() {
+                            loading = false;
+                          });
+                        }
+                    },
+                    
+                     
+                    child: const Text("Sign in",style: TextStyle(color: Colors.black87),),
+                        ),
+                    ),
+                    const SizedBox(height: 25,),
+                    const Text("Don't have account? Register below"),
+                    const SizedBox(height: 25,),
+                    Container(
+                      color: Colors.green,
+                      height: 50,
+                      width: MediaQuery.of(context).size.width,
+                      child: ElevatedButton(
+                        onPressed: () {
+                          widget.toggle();
+                        },
+                        child: const Text("Sign up",style: TextStyle(color: Colors.black87),),
+                        ),
+                    ),
+                    const SizedBox(height: 20,),
+                    GestureDetector(
+                      onTap: null,
+                      child: SizedBox(
+                        height: 60,
+                        width: 70,
+                        child: Image.asset("assets/gicon.png")
+                      ),
+                    ),
+                  ],
+                ),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
