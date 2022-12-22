b7e739e4b6372049b6cb252940a539178c166f1a
rahul <rs2570089@gmail.com>
2022-12-22 19:54:09 +0530

    Updated

--- lib/Home/conversation_screen.dart
+++ lib/Home/conversation_screen.dart
@@ -9,7 +9,8 @@ import 'package:encrypt/encrypt.dart' as encrypt;
 class ConversationScreen extends StatefulWidget {
   final String chatroomid;
   final String user;
-  const ConversationScreen(this.chatroomid,this.user, {Key? key}) : super(key: key);
+  const ConversationScreen(this.chatroomid, this.user, {Key? key})
+      : super(key: key);
 
   @override
   State<ConversationScreen> createState() => _ConversationScreenState();
@@ -19,47 +20,47 @@ class _ConversationScreenState extends State<ConversationScreen> {
   DataBaseMethods dataBaseMethods = DataBaseMethods();
   TextEditingController textEditingController = TextEditingController();
   Stream? chatmsg;
-  AbstractEncryption encryptionService = EncryptionService(encrypt.Encrypter(encrypt.AES(encrypt.Key.fromLength(32), padding: null)));
-
-  // String? key;
+  AbstractEncryption encryptionService = EncryptionService(encrypt.Encrypter(
+      encrypt.AES(encrypt.Key.fromLength(32), padding: null)));
 
   Widget chatMessageTile() {
     return StreamBuilder(
-      stream: chatmsg,
-      builder: (context,snapshot) {
-        if(!snapshot.hasData) {
-          return Container();
-        }
-        return ListView.builder(
-          itemCount: (snapshot.data as QuerySnapshot).docs.length,
-          itemBuilder: (context,index) {
-            return MessageTile(
-              encryptionService.decrypt((snapshot.data as QuerySnapshot).docs[index]["message"]),
-              (snapshot.data as QuerySnapshot).docs[index]["sender"] == constants.name 
-            );
+        stream: chatmsg,
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) {
+            return Container();
           }
-        );
-      }
-    );
+          return ListView.builder(
+              itemCount: (snapshot.data as QuerySnapshot).docs.length,
+              itemBuilder: (context, index) {
+                return MessageTile(
+                    encryptionService.decrypt((snapshot.data as QuerySnapshot)
+                        .docs[index]["message"]),
+                    (snapshot.data as QuerySnapshot).docs[index]["sender"] ==
+                        constants.name,
+                    (snapshot.data as QuerySnapshot).docs[index]["time"]);
+              });
+        });
   }
 
   sendMessages() {
-    if(textEditingController.text.isNotEmpty) {
-      Map<String,dynamic> msgmap = {
-        "message" : encryptionService.encrypt(textEditingController.text.trim()),
-        "sender" : constants.name,
-        "time" : "${DateTime.now().hour}:${DateTime.now().minute}",
-        "timeOrder" : DateTime.now().millisecondsSinceEpoch,
+    if (textEditingController.text.isNotEmpty) {
+      Map<String, dynamic> msgmap = {
+        "message": encryptionService.encrypt(textEditingController.text.trim()),
+        "sender": constants.name,
+        "time": DateTime.now().minute >= 0 && DateTime.now().minute <= 9
+            ? "${DateTime.now().hour}:${DateTime.now().minute}0"
+            : "${DateTime.now().hour}:${DateTime.now().minute}",
+        "timeOrder": DateTime.now().millisecondsSinceEpoch,
       };
-    dataBaseMethods.addConversationMsg(widget.chatroomid, msgmap);
-    textEditingController.text = "";
+      dataBaseMethods.addConversationMsg(widget.chatroomid, msgmap);
+      textEditingController.text = "";
     }
   }
-  
+
   @override
   void initState() {
     super.initState();
-    // key = encrypt.Key.fromUtf8(widget.chatroomid).toString();
     dataBaseMethods.getConversationMsg(widget.chatroomid).then((val) {
       setState(() {
         chatmsg = val;
@@ -67,19 +68,19 @@ class _ConversationScreenState extends State<ConversationScreen> {
     });
   }
 
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.user,style: const TextStyle(color: Colors.black),),
+        title: Text(
+          widget.user,
+          style: const TextStyle(color: Colors.black),
+        ),
         backgroundColor: Colors.green[50],
-          elevation: 0.0,
-          shadowColor: Colors.transparent,
-          centerTitle: true,
-          iconTheme: const IconThemeData(
-            color: Colors.green
-          ),
+        elevation: 0.0,
+        shadowColor: Colors.transparent,
+        centerTitle: true,
+        iconTheme: const IconThemeData(color: Colors.green),
       ),
       body: Container(
         color: Colors.green[100],
@@ -90,21 +91,21 @@ class _ConversationScreenState extends State<ConversationScreen> {
               alignment: Alignment.bottomCenter,
               child: Container(
                 color: const Color(0x54FFFFFF),
-                padding: const EdgeInsets.symmetric(horizontal: 24,vertical: 16),
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 24, vertical: 16),
                 child: Row(
                   children: [
                     Expanded(
-                      child: TextField(
-                        controller: textEditingController,
-                        decoration: const InputDecoration(
-                          hintText: "Message",
-                          hintStyle: TextStyle(
-                            color: Colors.grey,
-                          ),
-                          border: InputBorder.none,
+                        child: TextField(
+                      controller: textEditingController,
+                      decoration: const InputDecoration(
+                        hintText: "Message",
+                        hintStyle: TextStyle(
+                          color: Colors.grey,
                         ),
-                      )
-                    ),
+                        border: InputBorder.none,
+                      ),
+                    )),
                     SizedBox(
                       height: 40,
                       width: 40,
@@ -114,14 +115,17 @@ class _ConversationScreenState extends State<ConversationScreen> {
                           sendMessages();
                         },
                         backgroundColor: Colors.greenAccent,
-                        child: const Icon(Icons.send,),
+                        child: const Icon(
+                          Icons.send,
+                        ),
                       ),
                     ),
                   ],
                 ),
               ),
             ),
-        ],),
+          ],
+        ),
       ),
     );
   }
@@ -130,39 +134,57 @@ class _ConversationScreenState extends State<ConversationScreen> {
 class MessageTile extends StatelessWidget {
   final String msg;
   final bool sender;
-  const MessageTile(this.msg,this.sender, {Key? key}) : super(key: key);
+  final String time;
+  const MessageTile(this.msg, this.sender, this.time, {Key? key})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: const EdgeInsets.symmetric(vertical: 8),
+      margin: const EdgeInsets.symmetric(vertical: 7),
       alignment: sender ? Alignment.centerRight : Alignment.centerLeft,
-      width: MediaQuery.of(context).size.width,
-      padding: EdgeInsets.only(left: sender ? 0 : 20, right: sender ? 20 : 0),
+      padding: EdgeInsets.only(left: sender ? 0 : 10, right: sender ? 10 : 0),
       child: Container(
-        padding: const EdgeInsets.symmetric(horizontal: 24,vertical: 16),
+        padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 16),
+        constraints: BoxConstraints(
+          maxWidth: MediaQuery.of(context).size.width * 0.8,
+          minWidth: time.length.toDouble() * 15.5,
+        ),
         decoration: BoxDecoration(
-          gradient: LinearGradient(
-            colors: sender ? [Colors.green,Colors.green.shade400] : [Colors.white30,Colors.white70],
+            gradient: LinearGradient(
+              colors: sender
+                  ? [Colors.green, Colors.green.shade400]
+                  : [Colors.white30, Colors.white70],
+            ),
+            borderRadius: sender
+                ? const BorderRadius.only(
+                    topLeft: Radius.circular(20),
+                    topRight: Radius.circular(20),
+                    bottomLeft: Radius.circular(20))
+                : const BorderRadius.only(
+                    topLeft: Radius.circular(20),
+                    topRight: Radius.circular(20),
+                    bottomRight: Radius.circular(20))),
+        child: Stack(children: [
+          Padding(
+            padding: const EdgeInsets.only(bottom: 15),
+            child: Text(
+              msg,
+              style: const TextStyle(color: Colors.black, fontSize: 18),
+            ),
           ),
-          borderRadius: sender ? const BorderRadius.only(
-            topLeft: Radius.circular(20),
-            topRight: Radius.circular(20),
-            bottomLeft: Radius.circular(20)
-          ) : 
-          const BorderRadius.only(
-            topLeft: Radius.circular(20),
-            topRight: Radius.circular(20),
-            bottomRight: Radius.circular(20)
-          )
-        ),
-        child: Text(
-          msg,
-          style: const TextStyle(
-            color: Colors.black,
-            fontSize: 16
+          Positioned(
+            bottom: 0,
+            right: 0,
+            child: Align(
+              alignment: sender ? Alignment.topRight : Alignment.topLeft,
+              child: Text(
+                time,
+                style: const TextStyle(color: Colors.black, fontSize: 12),
+              ),
+            ),
           ),
-        ),
+        ]),
       ),
     );
   }
